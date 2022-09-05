c53907866066fb0f560f1fadd96d339743873e84
FedeCraviotto <fcraviotto.dev@gmail.com>
2022-09-05 17:20:00 -0300

    Adding filter --> search bar

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,9 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const productsFilePath = path.join(__dirname, '../database/products.json');
+
 const mainController = {
     index : (req, res) => {
-        res.render('main/index', {title:'Home'});
+        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        let lastVisited = products.filter(p => {return p.lastVisited === true})
+        res.render('main/index', { lastVisited, title:'Home'});
+        // Revisar cómo resolvemos el wrap de "te puede interesar" sin romper el responsive
     },
     search : (req, res) => {
-        res.render('main/results', {title:'Search results'});
+        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        let productsQuery = req.query.keywords.toLowerCase();
+        let queryResults = products.filter(p => {return p.shortDesc.toLowerCase().includes(productsQuery)})
+        res.render('main/results', {queryProducts : queryResults, productsQuery, title:'Search results'});
     }
 }
 module.exports = mainController;
