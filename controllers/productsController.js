add2fcac4ba5899bbb77c1efc1e5d46459be45a9
FedeCraviotto <fcraviotto.dev@gmail.com>
2022-09-05 13:16:19 -0300

    Adding products view. Dinamic details and list

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,12 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const productsFilePath = path.join(__dirname, '../database/products.json');
+
 const productsController = {
     list : (req, res) => {
-        res.render('products/products', {title:'All Products'});
+        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'))
+        res.render('products/products', {products, title:'All Products'});
     },
     create: (req, res) => {
         res.render('products/product-create', {title:'New Product'});
     },
     detail: (req, res) => {
-        res.render('products/product-detail', {title:'Detail'});
+        const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+        productToDetail = products.find(p => {return p.id == parseInt(req.params.id)});
+        res.render('products/product-detail', {product: productToDetail, title:'Detail'});
     },
     store: (req, res) => {
         //Todo
