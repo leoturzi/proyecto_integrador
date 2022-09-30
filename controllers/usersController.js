dc3613b8aed994a88f7d5e28090bb5c6f9a9ae04
leito <leogturzi@gmail.com>
2022-09-30 06:14:58 -0300

    Correccion logica de chequeo de errores en register

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,33 +3,47 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 const usersController = {
-  login: (req, res) => {
-    res.render('users/login', { title: 'Login' });
-  },
-  register: (req, res) => {
-    res.render('users/register', { title: 'Register' });
-  },
-  processRegister: (req, res) => {
-    // renderizado de errores
-    const results = validationResult(req).mapped();
-    if (results != undefined) {
-      res.render('users/register', {
-        title: 'Register',
-        errors: results,
-        oldData: req.body,
-      });
-    }
-    // creacion de usuario
-    let userToCreate = {
-      ...req.body,
-      password: bcrypt.hashSync(req.body.password, 10),
-      avatar: req.file.filename,
-    };
-    // agregar user al archivo users.jsons
-    User.create(userToCreate);
-  },
-  userProfile: (req, res) => {
-    res.render('users/userProfile', { title: 'Profile' });
-  },
+	login: (req, res) => {
+		res.render('users/login', { title: 'Login' });
+	},
+	register: (req, res) => {
+		res.render('users/register', { title: 'Register' });
+	},
+	processRegister: (req, res) => {
+		// renderizado de errores
+		const results = validationResult(req);
+		if (results.errors.length > 0) {
+			res.render('users/register', {
+				title: 'Register',
+				errors: results.mapped(),
+				oldData: req.body,
+			});
+		}
+		// validacion de que el email utilizado para el registro no esta en uso
+		const userInDb = User.findByField('email', req.body.email);
+
+		if (userInDb) {
+			return res.render('users/register', {
+				title: 'Register',
+				errors: { email: { msg: 'Este email ya esta registrado' } },
+				oldData: req.body,
+			});
+		}
+
+		// creacion de usuario
+		let userToCreate = {
+			...req.body,
+			password: bcrypt.hashSync(req.body.password, 10),
+			avatar: req.file.filename,
+		};
+		// agregar user al archivo users.jsons
+		User.create(userToCreate);
+
+		// return res.send('se guardo el usuario');
+		return res.redirect('/users/login');
+	},
+	userProfile: (req, res) => {
+		res.render('users/userProfile', { title: 'Profile' });
+	},
 };
 module.exports = usersController;
